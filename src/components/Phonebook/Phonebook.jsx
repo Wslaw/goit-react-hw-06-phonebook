28857ac8905a77abcfde2b87b1bb0038ee617b7a
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-02-09 07:54:39 +0200

    update

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -2,13 +2,10 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import image from '../forest.jpg';
-import { useSelector } from 'react-redux';
-import { getFilter } from '../../redux/filter/filter-selectors';
 
 import styles from './phonebook.module.css';
 
 const Phonebook = () => {
-  const filter = useSelector(getFilter);
 
   return (
     <div className={styles.body} style={{ backgroundImage: `url(${image})` }}>
@@ -18,7 +15,7 @@ const Phonebook = () => {
         <div>
           <h2 className={styles.title}>Contacts</h2>
           <p className={styles.text}>Find contacts by name</p>
-          <Filter value={filter} />
+          <Filter />
 
           <ContactList />
         </div>
